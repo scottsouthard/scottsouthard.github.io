dccf057d3a24a36b64178adc7a8dccd15c04015b
Scott Southard <scottmsouthard@gmail.com>
2016-04-07 15:58:00 +0000

    Remove unneccessary js

--- site.js
+++ site.js
@@ -11,14 +11,14 @@ $('nav a img').on('mouseleave', function(){
   }, 250);
 });
 
-$('header a').on('mouseover', function(){
-    $(this).animate({
-    'font-size': '3.1em',
-  }, 250);
-});
-$('header a').on('mouseleave', function(){
-    $(this).animate({
-    'font-size': '3em',
-  }, 250);
-});
+// $('header a').on('mouseover', function(){
+//     $(this).animate({
+//     'font-size': '3.1em',
+//   }, 250);
+// });
+// $('header a').on('mouseleave', function(){
+//     $(this).animate({
+//     'font-size': '3em',
+//   }, 250);
+// });
 });
