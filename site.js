ad75df3d7c88b4cb696041ad0724c702d515f2d8
Scott Southard <scottmsouthard@gmail.com>
2016-04-07 15:30:44 +0000

    Animate header

--- site.js
+++ site.js
@@ -10,4 +10,15 @@ $('nav a img').on('mouseleave', function(){
     'width': '40px',
   }, 250);
 });
+
+$('header a').on('mouseover', function(){
+    $(this).animate({
+    'font-size': '3.1em',
+  }, 250);
+});
+$('header a').on('mouseleave', function(){
+    $(this).animate({
+    'font-size': '3em',
+  }, 250);
+});
 });
